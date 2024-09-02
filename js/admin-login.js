9eb1e6251178df83c5c93878fbb6857a58c60204
clientesweb <alexisifran32@gmail.com>
2024-09-02 02:50:58 -0300

    Actualizar admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const mensajeError = document.getElementById('mensaje-error-admin');
     
     botonIniciarSesion.addEventListener('click', () => {
-        const usuario = document.getElementById('usuario-admin').value.trim();
-        const contrasena = document.getElementById('contrasena-admin').value.trim();
+        const usuario = 'admin' document.getElementById('usuario-admin').value.trim();
+        const contrasena = 'admin123' document.getElementById('contrasena-admin').value.trim();
 
         if (!usuario || !contrasena) {
             mensajeError.textContent = 'Por favor, complete todos los campos.';
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Aquí debes implementar la lógica de autenticación real.
         // Ejemplo básico:
-        if (usuario === '' && contrasena === '') {
+        if (usuario === 'admin' && contrasena === 'admin123') {
             // Redirigir a la página del panel de administrador
             window.location.href = 'admin.html';
         } else {
